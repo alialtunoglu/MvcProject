276762e4b948386e918cbf23b1e6c89d346bee56
Ali Altunoğlu <130387588+alialtunoglu@users.noreply.github.com>
2024-11-29 13:47:03 +0300

    add admin panel

---
+++ MvcProjeKampi/Controllers/AdminCategoryController.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        // GET: AdminCategory
+        public ActionResult Index()
+        {
+            var categoryvalues = cm.GetList();
+            return View(categoryvalues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            FluentValidation.Results.ValidationResult result = categoryValidator.Validate(p);
+            if (result.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
