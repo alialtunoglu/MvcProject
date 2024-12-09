fbc2dcf2332c0f4a7d5a9f43769220a36e53cf19
Ali Altunoğlu <130387588+alialtunoglu@users.noreply.github.com>
2024-12-09 17:42:44 +0300

    fix message page

---
+++ MvcProjeKampi/Controllers/MessageController.cs
@@ -0,0 +1,43 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class MessageController : Controller
+    {
+        MessageManager mm = new MessageManager(new EfMessageDal());
+        // GET: Message
+        public ActionResult Inbox()
+        {
+            var messageList = mm.GetListInbox();
+            var inboxCount = messageList.Count();
+            ViewBag.InboxCount = inboxCount;
+            return View(messageList);
+        }
+        public ActionResult Sendbox()
+        {
+            var messageList = mm.GetListSendbox();
+            var sendboxCount =messageList.Count();
+            ViewBag.SendboxCount = sendboxCount;
+            return View(messageList);
+        }
+        [HttpGet]
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewMessage(Message p)
+        {
+            return View();
+        }
+
+    }
+}
