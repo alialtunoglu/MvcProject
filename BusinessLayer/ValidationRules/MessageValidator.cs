331fb6d52ac380bd8a3f099575caa39ca5a9a7bb
Ali Altunoğlu <130387588+alialtunoglu@users.noreply.github.com>
2025-01-07 14:34:12 +0300

    added 404page

--- BusinessLayer/ValidationRules/MessageValidator.cs
+++ BusinessLayer/ValidationRules/MessageValidator.cs
@@ -15,7 +15,7 @@ namespace BusinessLayer.ValidationRules
             RuleFor(x => x.Subject).NotEmpty().WithMessage("Konuyu boş geçemezsiniz");
             RuleFor(x => x.MessageContent).NotEmpty().WithMessage("Mesajı boş geçemezsiniz");
             RuleFor(x => x.Subject).MinimumLength(3).WithMessage("Lütfen en az 3 karakter girişi yapın");
-            RuleFor(x => x.ReceiverMail).NotEmpty().WithMessage("E-posta adresi boş olamaz.").EmailAddress().WithMessage("Geçersiz e-posta adresi.");
+            RuleFor(x => x.ReceiverMail).NotEmpty().WithMessage("Alıcı E-posta adresi boş olamaz.").EmailAddress().WithMessage("Geçersiz e-posta adresi.");
             RuleFor(x => x.Subject).MaximumLength(100).WithMessage("Lütfen 20 karakterden fazla değer girişi yapmayın");
         }
     }
