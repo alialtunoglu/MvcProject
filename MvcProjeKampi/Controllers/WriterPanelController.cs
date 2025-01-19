6a3a9ba0a03b48cd0f0f01d17026ca515afa9a67
Ali Altunoğlu <alialtunoglu02@gmail.com>
2025-01-19 12:40:17 +0300

    added user add heading page

--- MvcProjeKampi/Controllers/WriterPanelController.cs
+++ MvcProjeKampi/Controllers/WriterPanelController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,18 +11,41 @@ namespace MvcProjeKampi.Controllers
 {
     public class WriterPanelController : Controller
     {
-        HeadingManager HeadingManager = new HeadingManager(new EfHeadingDal());
+        HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+
         // GET: WriterPanel
         public ActionResult WriterProfile()
         {
             return View();
         }
-        
+
         public ActionResult MyHeading()
         {
             
-            var headingvalues = HeadingManager.GetListByWriter();
+            var headingvalues = headingManager.GetListByWriter();
             return View(headingvalues);
+        }
+        [HttpGet]
+        public ActionResult NewHeading()
+        {
+            List<SelectListItem> valueCategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+            ViewBag.vlc = valueCategory;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewHeading(Heading p)
+        {
+            p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.WriterID = 12;
+            p.HeadingStatus = true;
+            headingManager.HeadingAdd(p);
+            return RedirectToAction("MyHeading");
         }
     }
 }
