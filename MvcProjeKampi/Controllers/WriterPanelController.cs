24ac26d0dba9468ceb29aea258fc83c2de4b6fdb
Ali Altunoğlu <alialtunoglu02@gmail.com>
2025-01-19 12:24:36 +0300

    added writerpanel page

---
+++ MvcProjeKampi/Controllers/WriterPanelController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class WriterPanelController : Controller
+    {
+        HeadingManager HeadingManager = new HeadingManager(new EfHeadingDal());
+        // GET: WriterPanel
+        public ActionResult WriterProfile()
+        {
+            return View();
+        }
+        
+        public ActionResult MyHeading()
+        {
+            
+            var headingvalues = HeadingManager.GetListByWriter();
+            return View(headingvalues);
+        }
+    }
+}
